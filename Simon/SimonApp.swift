796d6da714d30cfe9868ec862abd24485a99da64
Jack <Jwenschhof@bsd220.org>
2022-09-12 09:50:03 -0500

    Initial Commit

---
+++ Simon/SimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonApp.swift
+//  Simon
+//
+//  Created by Jack W on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SimonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
