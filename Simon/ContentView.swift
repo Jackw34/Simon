efa418427c52b2b364077e87f5ff444ba4c9bbd6
Jack <Jwenschhof@bsd220.org>
2022-09-14 09:53:30 -0500

    added sounds and changed text.

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello there!")
+        //(.preferredColorScheme(.dark))
+        Text("Simon")
+            .font(.system(size: 72))
             .padding()
     }
 }
