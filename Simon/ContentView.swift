5d8cebf3384e945b8420703c5605f5a1c7c83929
Jack <Jwenschhof@bsd220.org>
2022-10-03 09:45:05 -0500

    Made an array of colors

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -7,11 +7,21 @@
 
 import SwiftUI
 struct ContentView: View {
+    @State private var DisplayColor = [DisplayColor(color: .green), DisplayColor(color: .red), DisplayColor(color: .yellow), DisplayColor(color: .blue)]
     var body: some View {
         //(.preferredColorScheme(.black))
         Text("Simon")
             .font(.system(size: 72))
             .padding()
+    }
+    struct DisplayColor: View {
+        let color: Color
+        var body: some View {
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(color)
+                .frame(width: 100, height: 100, alignment: .center)
+                .padding()
+        }
     }
 }
 
