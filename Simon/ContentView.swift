0431e99c0b93c7397aebf7e5232560259eb3b824
Nerto Kageh <kagehnerto@yahoo.com>
2022-10-05 11:45:15 -0500

    Added flash color display. You can see a flash when you tap on one of the four buttons.

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -20,14 +20,29 @@ struct ContentView: View {
                 HStack {
                     colorDisplay[0]
                         .opacity(flash[0] ? 1 : 0.4)
+                        .onTapGesture {
+                            flashColorDisplay(index: 0)
+                        }
+                    
+                    
                     colorDisplay[1]
                         .opacity(flash[1] ? 1 : 0.4)
+                        .onTapGesture {
+                            flashColorDisplay(index: 1)
+                        }
                 }
                 HStack {
                     colorDisplay[2]
                         .opacity(flash[2] ? 1 : 0.4)
+                        .onTapGesture {
+                            flashColorDisplay(index: 2)
+                        }
+                    
                     colorDisplay[3]
                         .opacity(flash[3] ? 1 : 0.4)
+                        .onTapGesture {
+                            flashColorDisplay(index: 3)
+                        }
                 }
             }
         }
@@ -42,11 +57,15 @@ struct ContentView: View {
                 .padding()
         }
     }
-    
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+    func flashColorDisplay(index: Int) {
+        flash[index].toggle()
+        withAnimation(.easeInOut(duration: 0.5)) {
+            flash[index].toggle()
         }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
