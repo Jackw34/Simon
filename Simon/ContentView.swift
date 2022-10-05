6d3ece9801c5761323e1c778e2149a0d45ded6b5
Nerto Kageh <kagehnerto@yahoo.com>
2022-10-05 10:05:50 -0500

    Created HStacks for Simon Layout.

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -7,25 +7,37 @@
 import SwiftUI
 struct ContentView: View {
     @State private var colorDisplay = [ColorDisplay(color: .green), ColorDisplay(color: .red), ColorDisplay(color: .yellow), ColorDisplay(color: .blue)]
-    @State private var flash = [false, false, false, false] 
+    @State private var flash = [false, false, false, false]
     
     
     var body: some View {
-        Text("Simon")
-            .font(.system(size: 72))
-            .padding()
-            .preferredColorScheme(.dark)
-    }
-    struct ColorDisplay: View {
-        let color: Color
-        var body: some View {
-            RoundedRectangle(cornerRadius: 25.0)
-                .fill(color)
-                .frame(width: 100, height: 100, alignment: .center)
+            Text("Simon")
+                .font(.system(size: 72))
                 .padding()
+                .preferredColorScheme(.dark)
+        
+        HStack {
+            colorDisplay[0]
+            colorDisplay[1]
+        }
+        HStack {
+            colorDisplay[3]
+            colorDisplay[4]
         }
     }
 }
+
+
+struct ColorDisplay: View {
+    let color: Color
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(color)
+            .frame(width: 100, height: 100, alignment: .center)
+            .padding()
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
